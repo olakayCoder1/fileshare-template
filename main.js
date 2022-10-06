8b1a32166bf501e5ad91d6321be1a78997293e0d
olakayCoder1 <programmerolakay@gmail.com>
2022-10-07 00:11:13 +0100

    upload file function added

--- main.js
+++ main.js
@@ -10,3 +10,50 @@ function showNav() {
 }
 
 document.getElementById('nav-bar-burger').addEventListener('click',showNav)
+
+
+function addFolderName(e){
+    const folderName = prompt('Enter name of the folder')
+    if(folderName !== '' ) {
+        const url = 'blahblah'
+        // fetch(url, {
+        //     method : 'POST',
+        //     headers : {
+        //         'Content-type': 'application/json',
+        //     },
+        //     body : JSON.stringify({'folder_name' : folderName })
+        // })
+        // .then( res => res.json())
+        // .then( data => {
+        //     window.location = 'some url'
+        // })
+
+    }
+}
+
+function addFile(){
+    document.getElementById('file-input-upload').click()
+}
+
+const fileInput = document.getElementById('file-input-upload')
+
+fileInput.addEventListener('change', uploadFile = (e) => {
+    const file = document.getElementById('file-input-upload').files[0]
+    const name = file.name
+    const data = new FormData()
+    data.append('file', file , name)
+    console.log(data)
+    console.log('File uploaded successfully')
+// fetch(url, {
+    //     method : 'POST',
+    //     headers : {
+    //         'Content-type': 'application/json',
+    //     },
+    //     body : JSON.stringify({'folder_name' : folderName })
+    // })
+    // .then( res => res.json())
+    // .then( data => {
+    //     window.location = 'some url'
+    // })
+    }
+ )
